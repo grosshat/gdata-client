9df767210e55999a2f3ba02f26008a5fe3222b09
Iñigo Medina <inigo.medina@miraiespana.com>
2012-07-31 12:33:16 +0200

    First method for running samples.

---
+++ runner.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+# Copyright Iñigo Medina.
+
+"""Sample running."""
+
+__author__ = 'imedina@grosshat.com (Inigo Medina)'
+
+def Run(source_file):
+	"""Load a source file and run a sample from it."""
